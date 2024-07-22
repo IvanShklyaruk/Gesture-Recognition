5b1abaa08c879f9eef424f0a7c77fdc09673aef6
IvanShklyaruk <shklyaruk.ivan@gmail.com>
2024-07-22 01:57:15 -0400

    gesture recognition

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import GestureRecognitionPage from "./pages/GestureRecognitionPage";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="content">
+        <GestureRecognitionPage />
+      </div>
+    </div>
+  );
+}
+
+export default App;
